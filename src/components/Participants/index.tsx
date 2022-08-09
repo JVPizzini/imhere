4c357871ecdecfeaf2bc08edf7a0611eaadea385
João Vitor <joaovitorpizzini@hotmail.com>
2022-08-08 23:09:19 -0300

    feat: add e remove ok!

--- src/components/Participants/index.tsx
+++ src/components/Participants/index.tsx
@@ -3,11 +3,21 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
-export function Participants() {
+//interdaces and types
+type Props = {
+  name: string;
+  onRemoveName: (name: string) => void;
+};
+
+export function Participants({ name, onRemoveName }: Props) {
   return (
     <View style={styles.contextInput}>
-      <Text style={styles.text}>testando</Text>
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+      <Text style={styles.text}>{name}</Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={() => onRemoveName(name)}
+      >
         <Text style={styles.textButton}>-</Text>
       </TouchableOpacity>
     </View>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { styles } from "./styles";
 import { Participants } from "../../components/Participants";
 
 export default function Home() {
+  const [list, setList] = useState<string[]>([]);
+  const [listAux, setListAux] = useState<string[]>([]);
+  const [newName, setNewName] = useState("");
+  const input = useRef(null);
+
+  const onRemoveName = useCallback((name: string) => {
+    // const newList = list.filter((listName) => listName !== name);
+    // console.log(list.filter((listName) => listName !== name));
+    setList((prevState) => prevState.filter((listName) => listName !== name));
+  }, []);
+
+  const onAddName = useCallback((name: string) => {
+    if (name) {
+      setList((list) => [...list, name]);
+      setNewName("");
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(list);
+  }, [list]);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -14,19 +42,31 @@ export default function Home() {
 
       <View style={styles.contextInput}>
         <TextInput
+          ref={input}
           style={styles.textInput}
           placeholder="Digite algo..."
           placeholderTextColor="#6b6b6b"
+          onChangeText={setNewName}
+          value={newName}
         />
 
-        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={() => onAddName(newName)}
+        >
           <Text style={styles.textButton}>+</Text>
         </TouchableOpacity>
       </View>
-
-      <Participants />
-      <Participants />
-      <Participants />
+      {list.length > 0 && (
+        <FlatList
+          data={list}
+          keyExtractor={(item, i) => String(i)}
+          renderItem={({ item }) => (
+            <Participants name={item} onRemoveName={onRemoveName} />
+          )}
+        />
+      )}
     </View>
   );
 }
